ce82b431633f1b47e89d8181592208aedc18629c
prabirshrestha <mail@prabir.me>
2010-11-27 22:51:25 +0700

    + git prompt + alias for git.cmd and gitk.cmd

--- .profile.ps1
+++ .profile.ps1
@@ -62,6 +62,8 @@ Set-Alias np notepad++
 Set-Alias photoshop "$TOOLS\Photoshop\Photoshop.exe"
 Set-Alias reflector "$TOOLS\Reflector\reflector.exe"
 Set-Alias pik "$TOOLS\pik\pik.bat" # set aliaas for pik
+Set-Alias git "$TOOLS\git\cmd\git.cmd"
+Set-Alias gitk "$TOOLS\git\cmd\gitk.cmd"
 
 
 # uuidgen.exe replacement
@@ -69,5 +71,39 @@ function uuidgen {
    [guid]::NewGuid().ToString('d')
 }
 
-# import posh-git
+#************* START GIT *****************
+
+# Load posh-git module
 Import-Module $HOME\ps-scripts\posh-git\posh-git
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function prompt {
+    Write-Host($pwd) -nonewline
+        
+    # Git Prompt
+    $Global:GitStatus = Get-GitStatus
+    Write-GitStatus $GitStatus
+
+    return "> "
+}
+
+if(-not (Test-Path Function:\DefaultTabExpansion)) {
+    Rename-Item Function:\TabExpansion DefaultTabExpansion
+}
+
+# Set up tab expansion and include git expansion
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1]
+    
+    switch -regex ($lastBlock) {
+        # Execute git tab completion for all git-related commands
+        'git (.*)' { GitTabExpansion $lastBlock }
+        # Fall back on existing tab expansion
+        default { DefaultTabExpansion $line $lastWord }
+    }
+}
+
+Enable-GitColors
+
+Pop-Location
+#************* END GIT *****************
